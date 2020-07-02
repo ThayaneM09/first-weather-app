447d79caa07ce65459acf4a2fae0341a4e7a124c
ThayaneM09 <62439541+ThayaneM09@users.noreply.github.com>
2020-07-02 12:53:35 +0100

    no forecast

--- src/weather.js
+++ src/weather.js
@@ -69,45 +69,8 @@ function showWeather(response) {
 
     let weatherDescription = document.querySelector("#weatherDescription");
     weatherDescription.innerHTML = response.data.weather[0].main;
-    displayForecast(response);
 }
 
-function displayForecast(response) {
-    forecastItemMaxTemp = [];
-    forecastItemMinTemp = [];
-    let forecastElement = document.querySelector('#forecast');
-    forecastElement.innerHTML = null;
-    let forecast = null;
-
-    for (let index = 0; index < 6; index++) {
-        forecast = response.data.list[index];
-        forecastItemMaxTemp.push(Math.round(forecast.main.temp_max));
-        forecastItemMinTemp.push(Math.round(forecast.main.temp_min));
-
-        forecastElement.innerHTML +=
-            `<div class="row" id="nextDaysWeather">
-            <div class="col-2">
-            <img
-            src="http://openweathermap.org/img/wn/${
-            forecast.weather[0].icon
-            }@2x.png" width="40px"/>
-            </div>
-            <div class="col-4">
-                <h3 class="hours">
-                ${formatHours(forecast.dt * 1000)}
-                </h3>
-            </div>
-            <div class="col-6">
-                <p class="temp">
-                    <span class="tempMax">
-                ${Math.round(forecast.main.temp_max)}</span><span>º</span>            |
-                <span class="tempMin">${Math.round(forecast.main.temp_min)}</span><span>º</span>                
-                </p>
-            </div>
-        </div>`;
-    }
-
-}
 function searching(city) {
     let apiKey = `9f983349ddb8c26dfc6cae681695c977`;
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
@@ -145,20 +108,6 @@ function changingToFahreinheit(event) {
     let minTemperatureToday = document.querySelector('#tempMinToday');
     minTemperatureToday.innerHTML = `${Math.round(minFahrenheitTemperature)} ºF`;
 
-    let forecastMaxTemps = document.querySelectorAll(".tempMax");
-    forecastMaxTemps.forEach((forecastItem, index) => {
-        let forecastItemTemp = forecastItemMaxTemp[index];
-        return (forecastItem.innerHTML = Math.round(
-            (forecastItemTemp + 9) / 5 + 32
-        ));
-    });
-    let forecastMinTemps = document.querySelectorAll(".tempMin");
-    forecastMinTemps.forEach((forecastElement, index) => {
-        let forecastElementResult = forecastItemMinTemp[index];
-        return (forecastElement.innerHTML = Math.round(
-            (forecastElementResult + 9) / 5 + 32
-        ));
-    });
 }
 function changingToCelsius(event) {
     event.preventDefault();
@@ -171,14 +120,6 @@ function changingToCelsius(event) {
     celsiusLink.classList.add("active");
     fahrenheitLink.classList.remove("active");
 
-    let forecastMaxTemps = document.querySelectorAll(".tempMax");
-    forecastMaxTemps.forEach((forecastItem, index) => {
-        return (forecastItem.innerHTML = forecastItemMaxTemp[index]);
-    });
-    let forecastMinTemps = document.querySelectorAll(".tempMin");
-    forecastMinTemps.forEach((forecastElement, index) => {
-        return (forecastElement.innerHTML = forecastItemMinTemp[index]);
-    });
 }
 let searchCity = document.querySelector("#searchIcon");
 searchCity.addEventListener("click", handleSubmit);
@@ -197,8 +138,5 @@ form.addEventListener("submit", handleSubmit);
 
 let celsiusTemperature = null;
 let minCelsiusTemperature = null;
-
-let forecastItemMaxTemp = [];
-let forecastItemMinTemp = [];
 
 searching("Paris");
